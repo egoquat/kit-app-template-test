6453cd793cb47df93c7d8d1620564bd5c457feeb
egoquat <egohim@gmail.com>
2025-06-18 11:19:12 +0900

    add: test kit python pip packages

---
+++ source/extensions/test01.pip_prebundle_extension/docs/README.md
@@ -0,0 +1,3 @@
+# Test01 Pip Prebundle Extension [test01.pip_prebundle_extension]
+
+This is an example of pure python Kit extension. It is intended to be copied and to serve as a template to create new ones.
---
+++ source/extensions/test01.pip_prebundle_extension/premake5.lua
@@ -0,0 +1,12 @@
+-- Use folder name to build extension name and tag. Version is specified explicitly.
+local ext = get_current_extension_info()
+
+project_ext (ext)
+
+-- Link only those files and folders into the extension target directory
+repo_build.prebuild_link {
+    { "data", ext.target_dir.."/data" },
+    { "docs", ext.target_dir.."/docs" },
+    { "test01", ext.target_dir.."/test01" },
+	{ "%{root}/_build/target-deps/pip_prebundle", ext.target_dir.."/pip_prebundle" }, -- added line
+}
